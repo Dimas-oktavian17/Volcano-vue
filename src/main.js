47573a9f586386377aaae131077158808a76fdc4
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-12-20 14:16:32 +0700

    setup development, .env, tailwind, eslint, editorconfig, v-auto animate and iconift

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import './assets/main.css'
 
+import { Icon } from '@iconify/vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-app.use(router)
-
+app.component('IconVue', Icon)
+app.use(router).use(autoAnimatePlugin)
 app.mount('#app')
